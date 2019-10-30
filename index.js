87c472e1fe72ca8e382348b315214b0d30e0e2e5
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2019-10-30 17:42:10 -0400

    finished with mvp

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-document.querySelector('.tickets').onmouseover = function() {
+document.querySelector('.tickets').onclick = function() {
     document.querySelector('.ticketing').style.display ='block' 
     document.querySelector('.castle-text').style.display ='none'
     document.querySelector('.back').style.display ='block'
     document.querySelector('.tickets').style.display ='none'
 }
-document.querySelector('.back').onmouseout = function(){
+document.querySelector('.back').onclick = function(){
     document.querySelector('.ticketing').style.display = 'none'
     document.querySelector('.castle-text').style.display ='block'
     document.querySelector('.back').style.display ='none'
@@ -14,16 +14,17 @@ document.querySelector('.back').onmouseout = function(){
 let dragons = document.querySelector('.dragons')
 let info = document.querySelector('.info')
 let information = document.querySelector('.information')
+let dragonPic = document.querySelector('.dragon')
 let goBack = document.querySelector('.go-back')
 
-info.onmouseover = function(){
+info.onclick = function(){
 
     info.style.display ='none'
     dragons.style.display ='none'
     information.style.display ='block'
-    goBack.style.display ='block'
+    goBack.style.display = 'block'
 }
-goBack.onmouseout = function(){
+goBack.onclick= function(){
 
     info.style.display ='block'
     dragons.style.display ='block'
